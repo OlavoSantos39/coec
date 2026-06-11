65ede88c686362360cab844ad8b1931127028164
OlavoSantos39 <olavorps1@gmail.com>
2026-06-11 13:28:30 -0300

    oi

--- script/index.js
+++ script/index.js
@@ -9,7 +9,24 @@ document.getElementById("btnEntrar").addEventListener("click", function() {
     window.location.href = "principal.html";
 });
 
-
+const navLinks = document.querySelectorAll('#menu ul a.link');
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute
+            ('href'));
+            if (target) {
+                const headerHeight = document.querySelector('header').
+                offsetHeight;
+                const targetPosition = target.offsetTop - headerHeight - 
+                20;
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+    });
+});
 
 
 
